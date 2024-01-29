3646dca5181f3810db03656a636d435564e09b83
Anton Troshin <11165918+grauds@users.noreply.github.com>
2024-01-29 17:38:50 +0300

    [+] Added emoji favicon

--- src/server/indexTemplate.js
+++ src/server/indexTemplate.js
@@ -6,6 +6,7 @@ export const indexTemplate = (content) => `
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Pomodoro</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍅</text></svg>">
         <script src="/static/client.js" type="application/javascript"></script>
     </head>
     <body>
